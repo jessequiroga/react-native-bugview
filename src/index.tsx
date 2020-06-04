1d156fd2ee69f862bcdc673d4cf1c8a465bbb957
David Wang <devtech324@gmail.com>
2020-06-04 11:02:16 +0300

    fix

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import BugView from "./BugView";
+import withBugView, { WithBugView } from "./withBugView";
+import useBugView from "./useBugView";
+
+export {
+    withBugView,
+    WithBugView,
+    useBugView
+}
+
+export default BugView
